824e0dcedede08c88f242641457fdc757ebc947f
sergioncio <67414740+sergioncio@users.noreply.github.com>
2020-06-25 11:55:46 +0200

    Add files via upload

---
+++ Cajero.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroAutomatico
+{
+    class Cajero
+    {
+        private double saldo;
+
+        public Cajero(double saldo) 
+        {
+            this.saldo = saldo;
+        }
+
+        public double Ingresar(double dinero)
+        {
+           Console.WriteLine("Dinero ingresado con éxito.");
+           return this.saldo += dinero;
+        }
+
+        public double Retirar(double dinero)
+        {
+
+            if (dinero > saldo)
+            {
+                Console.WriteLine("El saldo disponible es inferior al dinero que quiere retirar, por lo tanto se le cobrará una comisión de 5 euros");
+                Console.WriteLine("¿Desea continuar con la operación? = 1-Sí 2-No");
+                int decision = Convert.ToInt16(Console.ReadLine());
+                if (decision == 1)
+                {
+                    saldo -= 5;
+                    Console.WriteLine("Dinero retirado con éxito.");
+                    return this.saldo -= dinero;
+                }
+                else if (decision == 2)
+                {
+                    return saldo;
+                }
+                else
+                {
+                    Console.WriteLine("Operación no disponible");
+                    return saldo;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Dinero retirado con éxito.");
+                return this.saldo -= dinero;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return this.saldo;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroAutomatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 0;
+            Boolean estado = true;
+            Cajero c = new Cajero(saldo);
+
+            while (estado) { 
+               Console.WriteLine("Seleccione la operación a realizar: 1-Ingresar dinero, 2-Retirar dinero, 3-Consultar saldo, 4-Finalizar transacciones");
+               int op = Convert.ToInt16(Console.ReadLine());
+
+                if (op == 1)
+                {
+                    Console.WriteLine("Introduzca la cantidad de dinero que quiere ingresar: ");
+                    double dinero = Convert.ToDouble(Console.ReadLine());
+                    c.Ingresar(dinero);  
+                }
+                else if (op == 2)
+                {
+                    Console.WriteLine("Introduzca la cantidad de dinero que quiere retirar: ");
+                    double dinero = Convert.ToDouble(Console.ReadLine());
+                    c.Retirar(dinero);
+                    Console.WriteLine();
+                }
+                else if (op == 3)
+                {
+                    Console.WriteLine("El saldo disponible en su cuenta es de: {0} euros \n", c.Saldo);
+                }
+                else if (op == 4)
+                {
+                    estado = false;
+                    c = null;
+                    GC.Collect();
+                }
+                else
+                {
+                    Console.WriteLine("La operación seleccionada no es válida. \n");
+                }
+            }
+            string a = Console.ReadLine();
+        }
+    }
+}
